75d67a7d1ef1cb20c7028a2741d5e1f6ec456041
Vignesh-7-7 <avicky8@gmail.com>
2025-10-12 11:52:25 +0530

    Add files via upload
    week1day1

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John Doe";
+		byte age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001, luckyNumber = 7;
+		System.out.println("Name ="+" "+name+"\n"+"Age ="+" "+age+"\n"+ "Salary ="+" "+salary+"\n"+"Gender ="+" "+gender+"\n"+"Accept Terms ="+" "+acceptTerms+"\n"+"Rating ="+" "+rating+"\n"+"Mobile Number ="+" "+mobileNumber+"\n"+"Pincode ="+" "+pinCode+"\n"+"Lucky Number ="+" "+luckyNumber);
+
+	}
+
+}
